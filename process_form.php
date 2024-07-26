29a478842e6990ada22e5c0fa0e89379637e75df
julietsummers <78687756+julietsummers@users.noreply.github.com>
2024-07-26 10:13:21 +0200

    Update process_form.php: email adderss update
    for php testing

--- process_form.php
+++ process_form.php
@@ -1,5 +1,5 @@
 <?php
-$to = 'my.email1@gmail.com';
+$to = 'juliet.summers1@gmail.com';
 $subject = 'New Form Submission';
 $message = "Name: {$_POST['name']}\nSurname: {$_POST['surname']}\nQuery: {$_POST['query']}";
 
